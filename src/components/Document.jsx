51a3addaf6b6cb864125c8eca7d16b6201d099a6
ol3gatron <olegatron@gmail.com>
2022-09-18 23:42:53 +0300

    added handleClick event in document component

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
+
 const Document = ({document}) => {
+  const [isHiden, setIsHiden] = useState(true)
+
+  const handleClick = () => {
+    setIsHiden(prevState => !prevState)
+  }
+
   return (
     <div className="document-item">
-      <div className="document-header">{document.header}</div>
-      <div className="document-body">
+      <div className="document-header" onClick={handleClick}>{document.header}</div>
+      {!isHiden &&
+        <div className="document-body">
         <p className="document-id">ID: {document.id}</p>
         <p className="document-date">Создан: {document.date}</p>
       </div>
+      }
     </div>
   )
 }
